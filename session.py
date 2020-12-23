3d79de2c6d6143535d83795bc611194cd108231f
Dave Meyer <unidavemeyer@gmail.com>
2020-12-23 15:18:41 -0800

    First pass session stuff
    Sessions are expected to each be in their own files found in a single
    session storage directory. Rather naive, but probably good enough for
    what we need for now.

--- session.py
+++ session.py
@@ -1,11 +1,90 @@
 # session-related machinery (tracks player state)
 
+import glob
+import yaml
+
 class Session:
     """Information about the state for a single player"""
 
     def __init__(self):
         self.m_uid = None
+        self.m_path = None
         self.m_room = None
+        self.m_roomSaved = None
+        self.m_mpVarVal = {}
+        self.m_fIsDirty = False
+
+    def Load(self, path):
+        """Serialize in this session from a file containing a yaml document"""
+
+        self.m_path = path
+
+        with open(path, 'r') as fileIn:
+            doc = yaml.safe_load(fileIn)
+
+        if doc is None:
+            return
+
+        self.m_uid = doc.get('uid')
+        self.m_roomSaved = doc.get('room')  # NOTE string, not room
+        self.m_mpVarVal = doc.get('vars')
+        self.m_fIsDirty = False
+
+    def Save(self):
+        """Serialize out this session as a yaml document to its source path"""
+
+        # TODO: is there a better way here? can we fake that we're a dict instead somehow?
+
+        dSelf = {}
+        dSelf['uid'] = self.m_uid
+        dSelf['room'] = self.m_room.m_name
+        dSelf['vars'] = self.m_mpVarVal
+
+        tmp = self.m_path + ".new"
+        with open(tmp, 'w') as fileOut:
+            yaml.serialize(dSelf, fileOut)
+
+        old = self.m_path + '.old'
+        os.remove(old)
+        os.rename(self.m_path, old)
+        os.rename(tmp, self.m_path)
+
+        self.m_fIsDirty = False
+
+    def StrErrors(self):
+        """Validate this session, and if it has problems, return a string explaining the issues"""
+
+        lStrErr = []
+
+        if self.m_uid is None:
+            lStrErr.append("UID is missing")
+
+        if self.m_path is None:
+            lStrErr.append("UID {u} missing its path".format(u=self.m_uid))
+
+        if self.m_roomSaved is None:
+            lStrErr.append("UID {u} missing its room".format(u=self.m_uid))
+
+        if not isinstance(self.m_mpVarVal, dict):
+            lStrErr.append("UID {u} has non-dictionary vars".format(u=self.m_uid))
+
+        # TODO: other things to check?
+
+        return '\n'.join(lStrErr)
+
+    def RoomCur(self):
+        return self.m_room
+
+    def SetRoomCur(self, room):
+        self.m_room = room
+        self.m_fIsDirty = True
+
+    def Var(self, var):
+        return self.m_mpVarVal.get(var, None)
+
+    def SetVar(self, var, value):
+        self.m_mpVarVal[var] = value
+        self.m_fIsDirty = True
 
 
 
@@ -15,6 +94,32 @@ class Group:
     def __init__(self):
         self.m_mpUidSession = {}
 
-    def Load(self):
-        # TODO
-        pass
+    def Load(self, pathDir):
+        """load all files from the directory, assuming they're sessions"""
+
+        for path in glob.glob("{p}/*".format(p=pathDir)):
+            session = Session()
+            session.Load(path)
+            strErrors = session.StrErrors()
+            if strErrors:
+                print("Session from {p} had errors:\n{e}".format(p=path, e=strErrors))
+            elif session.m_uid in self.m_mpUidSession:
+                print("Session {u} defined multiply (new {pNew}, prev {pOld})".format(
+                        u=session.m_uid,
+                        pNew=path,
+                        pOld=self.m_mpUidSession[session.m_uid].m_path))
+            else:
+                self.m_mpUidSession[session.m_uid] = session
+
+    def InitRooms(self, rooms):
+        """initialize actual room references for each session (typically after loading)"""
+
+        for session in self.m_mpUidSession.values():
+            room = rooms.Room(session.m_roomSaved)
+            session.SetRoomCur(room)
+
+if __name__ == '__main__':
+    # test driver for session stuff
+    group = Group()
+    group.Load('sampleSessions')
+    print("Finished loading sample sessions; errors will be listed above if any")
